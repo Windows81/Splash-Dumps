6cd0b09f7b4d16fe92729dd20ff6f1abef4a0e99
Windows81 <nastyahmede@gmail.com>
2020-11-30 09:20:33 +0000

    Improved play script.

--- play.ps1
+++ play.ps1
@@ -1,1 +1,8 @@
-$f=(New-TemporaryFile).FullName; $t=(($a|%{"-i $_"}) + "-filter_complex amix=inputs=$($a.Count):duration=shortest -y -f wav $f"); Start-Process ffmpeg ($t -join " ") -wait -nonewwindow; ffplay -loop -1 -f wav -i $f
+param(
+	[Parameter(Mandatory=$true)][string]$n,
+	[Parameter(Mandatory=$true)][uint]$r=44100,
+	[Parameter(Mandatory=$false)][string]$fl='anull')
+
+# $n="ABSTRACT"; $r=44100 # Example
+
+$e=$p=@();for($c=0;$c-lt48;$c++){$p+=($f=New-TemporaryFile);$a=ls dump -Name "$n*";$q=0;$a=(random -max 7 -min 0 -count 6)|%{$a[$_+6*($q++)]}; $t=(($a|%{"-i `"dump/$_`""}) + "-filter_complex amix=inputs=$($a.Count):duration=shortest,$fl,asetrate=$r -y -loglevel panic -f wav $f");$e+=(Start-Process ffmpeg ($t -join " ") -nonewwindow -passthru).id};Wait-Process -Id $e -ErrorAction SilentlyContinue;$f=New-TemporaryFile;$p|%{"file $($_.FullName.Replace("\","/"))"}|ffmpeg -safe 0 -protocol_whitelist "file,crypto,data,pipe" -f concat -i - -f wav $f -y -loglevel panic; ffplay $f -loop -1 -af volume=10dB
