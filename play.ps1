54d9f5706df8a5e2bd8a1a46fe928cc0d8e06c2c
Windows81 <nastyahmede@gmail.com>
2021-09-13 19:15:27 +0000

    Fixed play script again.

--- play.ps1
+++ play.ps1
@@ -1,8 +1,21 @@
 param(
-	[Parameter(Mandatory=$true)][string]$n,
-	[Parameter(Mandatory=$true)][uint]$r=44100,
-	[Parameter(Mandatory=$false)][string]$fl='anull')
+	[Parameter(Mandatory = $true)][string]$n,
+	[Parameter(Mandatory = $false)][uint]$sr = 44100,
+	[Parameter(Mandatory = $false)][string]$fl = 'anull')
 
-# $n="ABSTRACT"; $r=44100 # Example
+$e = $p = @()
+$l = Get-ChildItem dump -Name "$($n)_*"
+for ($c = $q = 0; $c -lt 48; $q = 0 * $c++) {
+	$p += ($f = New-TemporaryFile)
+	$r = Get-Random -min 0 -max 7 -count 6
+	$a = $r | ForEach-Object { $l[$_ + 20 * ($_ -ge 4) + 4 * ($q++)] }
+	$d = $a | ForEach-Object { "-i `"dump/$_`"" }
+	$t = "$d -filter_complex amix=inputs=$($a.Count):duration=shortest,$fl,asetrate=$sr -y -loglevel panic -f wav $f"
+	$e += (Start-Process ffmpeg ($t -join " ") -nonewwindow -passthru).id
+}
 
-$e=$p=@();for($c=0;$c-lt48;$c++){$p+=($f=New-TemporaryFile);$a=ls dump -Name "$n*";$q=0;$a=(random -max 7 -min 0 -count 6)|%{$a[$_+6*($q++)]}; $t=(($a|%{"-i `"dump/$_`""}) + "-filter_complex amix=inputs=$($a.Count):duration=shortest,$fl,asetrate=$r -y -loglevel panic -f wav $f");$e+=(Start-Process ffmpeg ($t -join " ") -nonewwindow -passthru).id};Wait-Process -Id $e -ErrorAction SilentlyContinue;$f=New-TemporaryFile;$p|%{"file $($_.FullName.Replace("\","/"))"}|ffmpeg -safe 0 -protocol_whitelist "file,crypto,data,pipe" -f concat -i - -f wav $f -y -loglevel panic; ffplay $f -loop -1 -af volume=10dB
+Wait-Process -Id $e -ErrorAction SilentlyContinue
+$f = New-TemporaryFile
+$p | ForEach-Object { "file $($_.FullName.Replace("\","/"))" } |
+ffmpeg -safe 0 -protocol_whitelist "file,crypto,data,pipe" -f concat -i - -f wav $f -y -loglevel panic
+ffplay $f -loop -1 -af volume=10dB
